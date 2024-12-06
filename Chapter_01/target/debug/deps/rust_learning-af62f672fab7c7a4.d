bc5e857a6f54276f1a055ea2c30f42874b38ccbb
Sergi <Sergih28@users.noreply.github.com>
2024-12-06 21:57:12 +0000

    WIP - feat(chapter 01): add a hello world!

---
+++ Chapter_01/target/debug/deps/rust_learning-af62f672fab7c7a4.d
@@ -0,0 +1,5 @@
+/home/sergi/Documents/Projects/Github/rust-learning/Chapter_01/target/debug/deps/librust_learning-af62f672fab7c7a4.rmeta: src/main.rs
+
+/home/sergi/Documents/Projects/Github/rust-learning/Chapter_01/target/debug/deps/rust_learning-af62f672fab7c7a4.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter_01/target/debug/deps/rust_learning-ef69107dd17df047.d
@@ -0,0 +1,5 @@
+/home/sergi/Documents/Projects/Github/rust-learning/Chapter_01/target/debug/deps/librust_learning-ef69107dd17df047.rmeta: src/main.rs
+
+/home/sergi/Documents/Projects/Github/rust-learning/Chapter_01/target/debug/deps/rust_learning-ef69107dd17df047.d: src/main.rs
+
+src/main.rs:
